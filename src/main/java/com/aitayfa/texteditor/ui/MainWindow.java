09ea9d671986f8ab1d519e94e3a6a7e22b42cdc7
Berkay Sarmasoğlu <berkaysarmasoglu2005@gmail.com>
2026-04-19 11:17:44 +0300

    redo and undo items becomes disabled when not possible

--- src/main/java/com/aitayfa/texteditor/ui/MainWindow.java
+++ src/main/java/com/aitayfa/texteditor/ui/MainWindow.java
@@ -222,6 +222,15 @@ public class MainWindow extends JFrame{
         menuEdit.add(itemClearHighlights);
         menuEdit.addSeparator();
         menuEdit.add(itemAI);
+        menuEdit.addMenuListener(new javax.swing.event.MenuListener() {
+            @Override
+            public void menuSelected(javax.swing.event.MenuEvent e) {
+                itemUndo.setEnabled(undoManager.canUndo());
+                itemRedo.setEnabled(undoManager.canRedo());
+            }
+            @Override public void menuDeselected(javax.swing.event.MenuEvent e) {}
+            @Override public void menuCanceled(javax.swing.event.MenuEvent e) {}
+        });
         
         // VIEW MENU
         JMenu menuView = uiFactory.createMenu("Görüntüle");
